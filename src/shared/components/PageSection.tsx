d49d681282aeb3297add95c435ce137ee4b8a21a
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-10 14:44:06 +0100

    feat: Implement PageSection component for consistent layout across all pages

---
+++ src/shared/components/PageSection.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from 'react';
+import { SEO } from './SEO';
+import { AutoCompleteSearchBar } from '../../pages/home/components/SeachBar';
+
+interface PageProps {
+  title: string;
+  seoTitle: string;
+  seoDescription: string;
+  searchBar?: boolean;
+  error?: string;
+  children: ReactNode;
+}
+
+function PageSection({ title, seoTitle, seoDescription, error, searchBar = false, children }: PageProps) {
+  return (
+    <section className="row-start-1 col-span-12 flex flex-col items-center">
+      <SEO title={seoTitle} description={seoDescription} />
+      {searchBar && (
+        <div className="w-full px-2 md:w-3/4 my-6">
+          <AutoCompleteSearchBar />
+        </div>
+      )}
+      <div className="max-w-screen-xl w-full mx-auto px-4 lg:px-8 flex flex-col items-center">
+        <div className="w-full text-left">
+          <h1 className="text-primary-dark-blue font-semibold text-md md:text-xl capitalize inline-block relative">
+            {title}
+            <span className="absolute left-0 bottom-0 h-1 w-full bg-accent-pink rounded-t-lg"></span>
+          </h1>
+        </div>
+
+        {error && (
+          <div role="alert" className="text-center text-status-error-red my-5 py-5">
+            {error}
+          </div>
+        )}
+
+        {children}
+      </div>
+    </section>
+  );
+}
+
+export default PageSection;
