57f369fe9c3711a33059aecd3b951ffbfe4f6efc
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-14 12:49:13 +0100

    feat: Add new API endpoints for login and register functionality

--- src/shared/utils/endpoints.ts
+++ src/shared/utils/endpoints.ts
@@ -1,6 +1,11 @@
 //storage API endpoints
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_KEY = import.meta.env.VITE_API_KEY || '__VITE_API_KEY__';
+
+export const API_REGISTER = '/register';
+export const API_LOGIN = '/login';
+export const API_AUTH = '/auth';
 
 export const API_VENUES = `${API_BASE_URL}/holidaze/venues`;
 export const API_VENUE = (id: string) => `${API_BASE_URL}/holidaze/venues/${id}`;
