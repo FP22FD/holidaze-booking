75fe7bdda14693f086b9cf85979f3f65655b66ad
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-14 12:46:37 +0100

    fix: Modify main Layout to hide header and footer on the AuthPage

--- src/shared/components/Layout.tsx
+++ src/shared/components/Layout.tsx
@@ -1,17 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 function Layout() {
+  const location = useLocation();
+  const shouldHideHeader = ['/auth/login', '/auth/register'].includes(location.pathname);
+
   return (
     <div className="min-h-screen grid grid-cols-12 grid-rows-[min-content_1fr_min-content]">
-      <Header />
+      {!shouldHideHeader && <Header />}
 
-      <main className="row-start-2 col-span-12 p-6 md:px-12 lg:px-16">
+      {/* <main className="row-start-2 col-span-12 px-4  md:px-12 lg:px-16"> */}
+      <main className="col-span-12">
         <Outlet />
       </main>
 
-      <Footer />
+      {!shouldHideHeader && <Footer />}
+      {/* <Footer /> */}
     </div>
   );
 }
