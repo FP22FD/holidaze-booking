d38d4747a107bdf4b500c6118721d0f154685bb1
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-16 14:30:06 +0100

    feat: Add new classes to manage button sizes in Button component

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -5,13 +5,25 @@ interface ButtonProps {
   onClick?: () => void;
   type: 'button' | 'submit' | 'reset';
   variant?: 'primary' | 'secondary' | 'default';
+  size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean; // New prop for full width
   disabled?: boolean;
   className?: string;
   to?: string;
 }
 
-const Button = ({ label, onClick, type, variant = 'primary', disabled = false, className = '', to }: ButtonProps) => {
-  const baseClasses = 'py-2 px-4 rounded transition-colors duration-200 font-bold';
+const Button = ({
+  label,
+  onClick,
+  type,
+  variant = 'primary',
+  size = 'medium',
+  fullWidth = false, // Default is not full width
+  disabled = false,
+  className = '',
+  to,
+}: ButtonProps) => {
+  const baseClasses = 'rounded transition-colors duration-200 font-bold';
 
   const variantClasses: { [key in 'primary' | 'secondary' | 'default']: string } = {
     primary: 'bg-pink-gradient hover:bg-pink-gradient-dark text-typography-primary-white',
@@ -19,21 +31,26 @@ const Button = ({ label, onClick, type, variant = 'primary', disabled = false, c
     default: 'bg-main-blue hover:bg-blue-primary-dark text-typography-primary-white text-center',
   };
 
+  const sizeClasses: { [key in 'small' | 'medium' | 'large']: string } = {
+    small: 'py-1 px-2 text-sm',
+    medium: 'py-2 px-4 text-base',
+    large: 'py-3 px-6 text-lg',
+  };
+
+  const fullWidthClass = fullWidth ? 'w-full' : '';
+
+  const combinedClasses = `${baseClasses} ${variantClasses[variant]} ${sizeClasses[size]} ${fullWidthClass} ${className}`;
+
   if (to) {
     return (
-      <NavLink to={to} className={`${baseClasses} ${variantClasses[variant]} ${className}`}>
+      <NavLink to={to} className={combinedClasses}>
         {label}
       </NavLink>
     );
   }
 
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      className={`${baseClasses} ${variantClasses[variant]} ${className}`}
-      disabled={disabled}
-    >
+    <button type={type} onClick={onClick} className={combinedClasses} disabled={disabled}>
       {label}
     </button>
   );
