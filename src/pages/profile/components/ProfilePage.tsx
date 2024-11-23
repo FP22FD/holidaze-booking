51b873c75ea9facd9a773c397691461e4c247ecd
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-23 16:20:24 +0100

    feat: Update Profile Page component for Improved Functionality and change name component

--- src/pages/profile/components/ProfilePage.tsx
+++ src/pages/profile/components/ProfilePage.tsx
@@ -3,8 +3,8 @@ import PageSection from '../../../shared/components/PageSection';
 import { useUpdateProfile } from '../hooks/useUpdateProfile';
 import { usePersistContext } from '../../../store/usePersistContext';
 import { useState } from 'react';
-import ProfileSection from './ProfileSection';
 import ProfileForm from './ProfileForm';
+import UpcomingBookings from './UpcomingBookings';
 
 function ProfilePage() {
   const { userType, profileData, requireUserLevel } = usePersistContext();
@@ -18,7 +18,7 @@ function ProfilePage() {
     return;
   }
 
-  const { name, avatar, bio } = profileData;
+  const { name, avatar, bio, email } = profileData;
 
   return (
     <PageSection
@@ -30,9 +30,33 @@ function ProfilePage() {
       {loading && <div className="text-typography-primary-blue">Updating profile...</div>}
       {error && <div className="text-status-error-red">{error}</div>}
 
-      <div className="w-full mx-auto flex flex-col md:flex-row md:space-x-8 lg:space-x-16 rounded-lg py-2 ">
+      <div className="w-full mx-auto flex flex-col md:flex-row md:space-x-8 lg:space-x-16 rounded-lg py-2">
         <div className="w-full md:w-1/3 flex flex-col h-fit">
           <div className="w-fit p-2 bg-white border my-2 rounded-lg">
+            <div className="avatar-section mb-6 ">
+              <div className="flex items-center space-x-2">
+                <div className="w-24 h-24">
+                  <img src={avatar?.url} alt="Profile Avatar" className="w-full h-full object-cover rounded" />
+                </div>
+
+                <div className="flex flex-col text-md">
+                  <p className="font-semibold text-primary-dark-blue">{name}</p>
+                  <p className="text-body-small">{email}</p>
+                  <div className="flex space-x-6 text-body-small"></div>
+                  <p className="text-typography-primary-grey text-body-small">{userType}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              {/* TODO: Add bookings and venues count with Update profile fetch */}
+              {/* <p>{_count.bookings}</p>
+              <p>{_count.venues}</p> */}
+            </div>
+
+            <div className="text-typography-primary-blue mb-6">
+              {bio || 'Bio not available yet. Update your profile to add a bio.'}
+            </div>
+
             <div className="place-self-end">
               {!isEditing && (
                 <div>
@@ -46,27 +70,12 @@ function ProfilePage() {
                 </div>
               )}
             </div>
-
-            <div className="avatar-section">
-              <div className="flex items-center space-x-2">
-                <div className="w-24 h-24">
-                  <img src={avatar?.url} alt="Profile Avatar" className="w-full h-full object-cover rounded-full" />
-                </div>
-
-                <div className="flex flex-col text-md">
-                  <span className="font-semibold text-primary-dark-blue">{name}</span>
-                  <span className="text-typography-primary-grey text-body-medium">{userType}</span>
-                </div>
-              </div>
-            </div>
-            <div className="text-typography-primary-blue">
-              {bio || 'Bio not available yet. Update your profile to add a bio.'}
-            </div>
           </div>
+
           {isEditing && <ProfileForm onClose={() => setIsEditing(false)} />}
         </div>
         <div className="w-full md:w-2/3 border rounded-lg p-2">
-          <ProfileSection />
+          <UpcomingBookings name={name} />
         </div>
       </div>
     </PageSection>
