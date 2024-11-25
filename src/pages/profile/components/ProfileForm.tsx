4316ac60eadc9b0457cd4bd132a5bd4e76a2f5e2
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-25 15:14:19 +0100

    Refactor: Update profile form to improve YUP validation and display modal feedback UI

--- src/pages/profile/components/ProfileForm.tsx
+++ src/pages/profile/components/ProfileForm.tsx
@@ -1,24 +1,47 @@
-import { PiXLight } from 'react-icons/pi';
+import { PiCheckCircleFill, PiXLight } from 'react-icons/pi';
 import Button from '../../../shared/components/Button';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { usePersistContext } from '../../../store/usePersistContext';
 import { useUpdateProfile } from '../hooks/useUpdateProfile';
+import { useState } from 'react';
+import { ModalMessage } from '../../../shared/components/Modal';
 
 interface ProfileFormProps {
   onClose: () => void;
 }
 
+const validateUrl = async (url: string) => {
+  try {
+    const response = await fetch(url, { method: 'HEAD' });
+    if (!response.ok) {
+      return new Yup.ValidationError('The URL is not publicly accessible.', null, 'avatarUrl');
+    }
+    return true;
+  } catch {
+    return new Yup.ValidationError('The URL is not valid or accessible.', null, 'avatarUrl');
+  }
+};
+
 const validationSchema = Yup.object({
-  avatarUrl: Yup.string().url().required('Please enter your avatar url'),
-  avatarAlt: Yup.string().required('Please enter your xxx').min(8, 'Password must be at least 8 characters'),
-  bio: Yup.string().required('Please enter your password').min(8, 'Password must be at least 8 characters'),
+  avatarUrl: Yup.string()
+    .trim()
+    .url('Please enter a valid URL for your avatar')
+    .required('Please enter your avatar url')
+    .test('check-url', 'The URL is not valid or accessible.', validateUrl),
+  avatarAlt: Yup.string()
+    .trim()
+    .required('Please enter the image description')
+    .min(8, 'Image description must be at least 4 characters'),
+  bio: Yup.string().trim().required('Please enter your bio').min(8, 'Bio must be at least 8 characters'),
 }).required();
 
 const ProfileForm = ({ onClose }: ProfileFormProps) => {
   const { profileData, setProfileData } = usePersistContext();
-  const { updateProfile } = useUpdateProfile();
+  const { loading, updateProfile } = useUpdateProfile();
+  const [formError, setFormError] = useState<string>('');
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
   const {
     register,
@@ -40,23 +63,42 @@ const ProfileForm = ({ onClose }: ProfileFormProps) => {
     const { success, data: updatedProfileData } = await updateProfile(newProfile);
 
     if (!success) {
+      setFormError('Failed to update profile. Please try again later.');
       return;
     }
 
     if (success && updatedProfileData) {
       setProfileData(updatedProfileData);
-      onClose();
+      setIsSuccess(true);
+      console.log(isSuccess);
     }
   };
 
   return (
     <>
-      <form className="p-2 bg-white border my-2 rounded-lg" onSubmit={handleSubmit(onSubmit)}>
-        <div className="p-6 flex justify-end">
-          <button aria-label="Close menu" onClick={onClose} className="focus:outline-none bg-pink-gradient rounded p-1">
-            <PiXLight className="w-6 h-6 text-white" aria-hidden="true" />
+      {isSuccess && (
+        <ModalMessage
+          onClose={onClose}
+          message="You have successfully updated your profile!"
+          icon={<PiCheckCircleFill className="w-16 h-16 text-primary-dark-blue" />}
+        />
+      )}
+
+      <form className="p-4 bg-neutral-white border my-2 rounded-lg" onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="mb-6 flex justify-end">
+          <button
+            aria-label="Close menu"
+            onClick={(e) => {
+              e.preventDefault();
+              onClose();
+            }}
+            className="focus:outline-none bg-pink-gradient rounded p-1"
+          >
+            <PiXLight className="w-6 h-6 text-typography-primary-white" aria-hidden="true" />
           </button>
         </div>
+
+        {formError && <div className="text-status-error-red text-center my-4">{formError}</div>}
 
         <div className="place-self-start w-full space-y-4 md:space-y-6 lg:space-y-8 text-body-medium sm:text-body-large">
           <div>
@@ -65,9 +107,10 @@ const ProfileForm = ({ onClose }: ProfileFormProps) => {
             </label>
             <input
               id="avatarUrl"
-              type="name"
+              type="url"
               {...register('avatarUrl')}
               placeholder="Enter the image url"
+              aria-describedby={errors.avatarUrl ? 'avatarUrlError' : undefined}
               className="w-full border rounded-md p-2 mt-1 focus:outline-none focus:ring-1 focus:ring-offset-primary-light-blue"
             />
             {errors.avatarUrl && <div className="text-status-error-red">{errors.avatarUrl.message}</div>}
@@ -82,6 +125,7 @@ const ProfileForm = ({ onClose }: ProfileFormProps) => {
               type="text"
               {...register('avatarAlt')}
               placeholder="Enter your image name"
+              aria-describedby={errors.avatarAlt ? 'avatarAltError' : undefined}
               className="w-full border rounded-md p-2 mt-1 focus:outline-none focus:ring-1 focus:ring-offset-primary-light-blue"
             />
             {errors.avatarAlt && <div className="text-status-error-red">{errors.avatarAlt.message}</div>}
@@ -96,6 +140,7 @@ const ProfileForm = ({ onClose }: ProfileFormProps) => {
               type="text"
               {...register('bio')}
               placeholder="Enter your bio"
+              aria-describedby={errors.bio ? 'bioError' : undefined}
               className="w-full border rounded-md p-2 mt-1 focus:outline-none focus:ring-1 focus:ring-offset-primary-light-blue"
             />
 
@@ -103,7 +148,7 @@ const ProfileForm = ({ onClose }: ProfileFormProps) => {
           </div>
 
           <div className="flex space-x-8 place-self-center">
-            <Button type="submit" label="Update" size="medium" variant="primary" />
+            <Button type="submit" label="Update" size="medium" variant="primary" disabled={loading} />
           </div>
         </div>
       </form>
