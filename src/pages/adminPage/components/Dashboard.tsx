91679b297a010d8e4c309c49e106a67091a06015
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-28 14:20:42 +0100

    refactor: update component to display management data

--- src/pages/adminPage/components/Dashboard.tsx
+++ src/pages/adminPage/components/Dashboard.tsx
@@ -1,12 +1,33 @@
 import PageSection from '../../../shared/components/PageSection';
+import Spinner from '../../../shared/components/Spinner';
 import { usePersistContext } from '../../../store/usePersistContext';
+import { useVenueManagement } from '../hooks/useDashboard';
 import DashboardCards from './DashboardCards';
 import ManageVenues from './ManageVenues';
 import VenueBookings from './VenueBookings';
 
 function Dashboard() {
-  const { requireUserLevel } = usePersistContext();
+  const { requireUserLevel, profileData } = usePersistContext();
+  const { error, loading, stats, venuesData, loadVenues } = useVenueManagement(profileData?.name);
+  console.log('venuesData', venuesData);
+
   requireUserLevel('customer');
+
+  if (loading) {
+    return (
+      <div className="text-typography-primary-blue">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="text-status-error-red">{error}</div>;
+  }
+
+  if (!stats) {
+    return;
+  }
 
   return (
     <PageSection
@@ -16,10 +37,10 @@ function Dashboard() {
       searchBar={false}
     >
       <div className="w-full mx-auto flex flex-col md:flex-row md:space-x-8 lg:space-x-16 py-2">
-        <div className="w-full space-y-8 border rounded-lg">
-          <DashboardCards />
-          <ManageVenues />
-          <VenueBookings />
+        <div className="w-full space-y-8 border rounded-lg pb-10">
+          <DashboardCards stats={stats} />
+          <ManageVenues venues={venuesData ?? []} onRefresh={() => loadVenues()} />
+          <VenueBookings venues={venuesData ?? []} />
         </div>
       </div>
     </PageSection>
