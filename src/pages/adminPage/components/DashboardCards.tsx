0cf57edc6ef0ba79e54a4c236d6242eee63ce694
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-28 14:25:24 +0100

    refactor: update component to display data in dashboard section overview

--- src/pages/adminPage/components/DashboardCards.tsx
+++ src/pages/adminPage/components/DashboardCards.tsx
@@ -1,15 +1,21 @@
-const DashboardCards = () => {
+import { AdminVenuesStats } from '../hooks/useDashboard';
+
+interface Props {
+  stats: AdminVenuesStats;
+}
+
+const DashboardCards = ({ stats }: Props) => {
   return (
     <div className="m-2">
       <div>
         <div className="flex justify-between">
           <div className="border rounded-lg p-10 m-2 text-center w-full">
-            <p className="font-semibold text-neutral-dark">Venues</p>
-            <span className="text-primary-dark-blue font-bold text-lead-paragraph">1</span>
+            <p className="font-semibold text-typography-primary-grey">Venues</p>
+            <span className="text-primary-dark-blue font-bold text-lead-paragraph">{stats.venues}</span>
           </div>
           <div className="border rounded-lg p-10 m-2 text-center w-full">
-            <p className="font-semibold text-neutral-dark">Bookings</p>
-            <span className="text-primary-dark-blue font-bold text-lead-paragraph">1</span>
+            <p className="font-semibold text-typography-primary-grey">Bookings</p>
+            <span className="text-primary-dark-blue font-bold text-lead-paragraph">{stats.bookings}</span>
           </div>
         </div>
       </div>
