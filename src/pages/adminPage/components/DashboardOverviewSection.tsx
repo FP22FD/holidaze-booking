dc24cb3f476dc0958add98736a871e26c53e6180
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-11-29 22:24:14 +0100

    refactor: renamed file in manager dashboard section overview

--- src/pages/adminPage/components/DashboardOverviewSection.tsx
+++ src/pages/adminPage/components/DashboardOverviewSection.tsx
@@ -4,16 +4,16 @@ interface Props {
   stats: AdminVenuesStats;
 }
 
-const DashboardCards = ({ stats }: Props) => {
+const StatOverviewCards = ({ stats }: Props) => {
   return (
     <div className="m-2">
       <div>
         <div className="flex justify-between">
-          <div className="border rounded-lg p-10 m-2 text-center w-full">
+          <div className="border rounded-lg p-8 m-2 text-center w-full">
             <p className="font-semibold text-typography-primary-grey">Venues</p>
             <span className="text-primary-dark-blue font-bold text-lead-paragraph">{stats.venues}</span>
           </div>
-          <div className="border rounded-lg p-10 m-2 text-center w-full">
+          <div className="border rounded-lg p-8 m-2 text-center w-full">
             <p className="font-semibold text-typography-primary-grey">Bookings</p>
             <span className="text-primary-dark-blue font-bold text-lead-paragraph">{stats.bookings}</span>
           </div>
@@ -23,4 +23,4 @@ const DashboardCards = ({ stats }: Props) => {
   );
 };
 
-export default DashboardCards;
+export default StatOverviewCards;
