0bde7545f6ef1a2749cfebe6b6d2d5802b60469b
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-03 02:03:16 +0100

    feat: update error handling for fetch with 409 conflict status

--- src/shared/utils/errorHandler.ts
+++ src/shared/utils/errorHandler.ts
@@ -14,7 +14,6 @@ export class ErrorHandler {
 
     if (this._response.status === 400) {
       // 400 - Bad request
-      /** @type {BadRequestResponse} */
       const data = await this._response.json();
       errorMessage = data.errors[0].message;
     } else if (this._response.status === 401) {
@@ -23,6 +22,10 @@ export class ErrorHandler {
     } else if (this._response.status === 404) {
       // 404 - Not Found
       errorMessage = 'The requested resource was not found!';
+    } else if (this._response.status === 409) {
+      // 409 - Conflict
+      const data = await this._response.json();
+      errorMessage = data.errors[0].message;
     } else {
       // 500 - Internal Server Error and other server-side errors
       errorMessage = 'Unknown error! Please retry later.';
