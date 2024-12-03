8aabe5d554c633c1fe246a7cb98594761964bc65
FP22FD <112474910+FP22FD@users.noreply.github.com>
2024-12-03 01:52:41 +0100

    feat: update VenueCard component to booking calendar implementation

--- src/pages/venue/components/VenueCard.tsx
+++ src/pages/venue/components/VenueCard.tsx
@@ -3,16 +3,19 @@ import PageSection from '../../../shared/components/PageSection';
 import { useFetchVenue } from '../hooks/fetchVenue';
 import StarRating from '../../../shared/components/StarRating';
 import { PiCarLight, PiCoffeeLight, PiMapPinLight, PiPawPrintLight, PiWifiHighLight } from 'react-icons/pi';
-import Button from '../../../shared/components/Button';
 import { Facility } from './Facility';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import VenueBookingCalendar from './VenueBookingCalendar';
+import Spinner from '../../../shared/components/Spinner';
 
 function VenueCard() {
   const { id } = useParams<{ id: string }>();
-  const { data, error } = useFetchVenue(id || '');
+  const { data: venue, error } = useFetchVenue(id || '');
 
-  const { media, location, name, description, rating, maxGuests, price, meta } = data || {};
+  if (!venue) {
+    return <Spinner />;
+  }
+
+  const { media, location, name, description, rating, maxGuests, meta } = venue;
   const imageUrl = media?.length ? media[0].url : '';
   const altText = media?.length ? media[0].alt : 'Venue Image';
   const locationCountry = location?.country || 'Location unavailable';
@@ -64,82 +67,10 @@ function VenueCard() {
             {meta?.pets && <Facility icon={<PiPawPrintLight />} label="Pets Allowed" />}
             {meta?.breakfast && <Facility icon={<PiCoffeeLight />} label="Breakfast" />}
           </div>
-
-          {/* Calendar Display Under Confirm Booking Button */}
-          <div className="flex justify-start items-start flex-col bg-neutral-white px-2 my-8">
-            <p className="font-semibold mb-4">Ours Available dates</p>
-            <div className="border-t border-neutral-default mb-3"></div>
-
-            <div className="bg-neutral-white rounded-lg ">
-              <DatePicker
-                selected={null}
-                onChange={() => {}}
-                inline
-                calendarClassName="rounded-lg"
-                dayClassName={(date) =>
-                  `text-typography-primary-grey rounded-full ${
-                    date.getDate() === new Date().getDate() ? 'bg-pink-gradient text-white' : ''
-                  } hover:neutral-default hover:text-typography-primary-grey e hover:rounded-full`
-                }
-              />
-            </div>
-          </div>
         </div>
 
         <div className="w-full md:w-1/3">
-          <div className="max-w-md p-6 bg-white shadow-md rounded-lg">
-            <h2 className="text-2xl font-bold text-center mb-6 text-primary-dark-blue">Booking</h2>
-
-            <form className="space-y-4">
-              <div>
-                <label htmlFor="from" className="block text-typography-primary-blue font-semibold">
-                  From
-                </label>
-                <input
-                  id="from"
-                  type="date"
-                  className="w-full border rounded-md p-2 mt-1 focus:outline-none focus:ring-1 focus:ring-offset-primary-light-blue"
-                />
-              </div>
-
-              <div>
-                <label htmlFor="to" className="block text-typography-primary-blue font-semibold">
-                  To
-                </label>
-                <input
-                  id="to"
-                  type="date"
-                  className="w-full border rounded-md p-2 mt-1 focus:outline-none focus:ring-1 focus:ring-offset-primary-light-blue"
-                />
-              </div>
-              <div>
-                <label htmlFor="guests" className="block text-typography-primary-blue font-semibold">
-                  No. of Guests
-                </label>
-                <input
-                  id="guests"
-                  type="number"
-                  min={1}
-                  className="w-full border rounded-md p-2 mt-1 focus:outline-none focus:ring-1 focus:ring-offset-primary-light-blue"
-                />
-              </div>
-
-              <div className="text-center my-4 flex flex-col pb-4">
-                <p className="text-body-large text-typography-primary-grey font-medium mb-4">Subtotal</p>
-                <p className="text-accent-pink font-extrabold text-4xl">${price}</p>
-              </div>
-              <div className="w-full place-self-center">
-                <Button
-                  label="Confirm Booking"
-                  type="button"
-                  ariaLabel="Confirm booking"
-                  variant="primary"
-                  size="large"
-                  fullWidth={true}
-                />
-              </div>
-            </form>
-          </div>
+          <VenueBookingCalendar venue={venue} />
         </div>
       </div>
     </PageSection>
